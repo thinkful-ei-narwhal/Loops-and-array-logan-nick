3f79e0920dd915733ee5b79e223fd959f9574b53
Logan Stribling <logan.stribling@gmail.com>
2020-03-17 11:18:55 -0600

    second drill

--- drills.js
+++ drills.js
@@ -1,6 +1,14 @@
-//Redo the "max and min (without sort or Math.max/min)" drill 
-//but this time use a while loop instead of a for loop
-'use strict';
+let numArray = [14, 19, 33, 55];
+
+function average(numbers) {
+  let total = 0;
+  numbers.forEach(x => (total += x));
+  return total / numbers.length;
+}
+
+console.log(average(numArray));
+
+("use strict");
 
 function max(numbers) {
   if (numbers.length < 1) {
@@ -25,17 +33,68 @@ function min(numbers) {
   }
 
   let smallest = numbers[0];
-  let i = 0;
-  //while
-  while (i < numbers.length) {
-    if (numbers[i] < smallest) {
-      smallest = numbers[i];
-    }
-    i++;
-  }
+  let i = 0; //while while (i < numbers.length) { if (numbers[i] < smallest) { smallest = numbers[i]; } i++; }
 
   return smallest;
 }
 
 console.log(max([3, 10, 27, 2]));
 console.log(min([3, 10, 27, 2]));
+
+("use strict");
+
+//
+let numArray = [14, 19, 33, 55];
+function average(numbers) {
+  //add all numbers and divide by array length.
+  let total = 0;
+  numbers.forEach(x => (total += x));
+  return total / numbers.length;
+}
+
+console.log(average(numArray));
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testFunctionWorks(fn, input, expected) {
+  if (fn(input) === expected) {
+    console.log("SUCCESS: `" + fn.name + "` works on `[" + input + "]`");
+    return true;
+  } else {
+    console.log(
+      "FAILURE: `" +
+        fn.name +
+        "([" +
+        input +
+        "])` should be " +
+        expected +
+        " but was " +
+        fn(input)
+    );
+    return false;
+  }
+}
+
+(function runTests() {
+  const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const correctAns1 = 5.5;
+  const numList2 = [0, -1, 1];
+  const correctAns2 = 0;
+
+  const testResults = [
+    testFunctionWorks(average, numList1, correctAns1),
+    testFunctionWorks(average, numList2, correctAns2)
+  ];
+  const numPassing = testResults.filter(function(result) {
+    return result;
+  }).length;
+  console.log(numPassing + " out of " + testResults.length + " tests passing.");
+})();
